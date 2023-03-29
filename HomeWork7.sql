8933a7e4e4a1dae410f1f59400ae5b0d67b69baa
Murat ARSLAN <68780096+MuratArs1an@users.noreply.github.com>
2023-03-29 23:19:29 +0300

    Add files via upload

---
+++ HomeWork7.sql
@@ -0,0 +1,18 @@
+1-
+SELECT COUNT(title),rating FROM film 
+GROUP BY rating;
+
+2-
+SELECT COUNT(title),replacement_cost FROM film
+GROUP BY replacement_cost
+HAVING COUNT(title)>50;
+
+3-
+SELECT COUNT(customer_id) FROM customer
+GROUP BY store_id;
+
+4-
+SELECT country_id, COUNT(city) FROM city
+GROUP BY country_id
+ORDER BY COUNT(city) DESC
+LIMIT 1;
