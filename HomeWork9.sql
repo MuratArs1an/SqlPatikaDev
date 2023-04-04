cd38104d69dc063e30712cf2b41d0589fad4f753
Murat ARSLAN <68780096+MuratArs1an@users.noreply.github.com>
2023-04-04 23:12:16 +0300

    Add files via upload

---
+++ HomeWork9.sql
@@ -0,0 +1,12 @@
+1-
+SELECT city, country from city
+
+INNER JOIN country ON city.country_id=country.country_id;
+
+2-
+SELECT payment_id, first_name, last_name from payment
+INNER JOIN customer ON payment.customer_id=customer.customer_id;
+
+3-
+SELECT rental_id, first_name, last_name from rental
+INNER JOIN customer ON rental.customer_id=customer.customer_id;
