655fdc7f2a1e58497f8ac66c3f9d367af2f285dc
Murat ARSLAN <68780096+MuratArs1an@users.noreply.github.com>
2023-04-05 22:44:31 +0300

    Add files via upload

---
+++ HomeWork11.sql
@@ -0,0 +1,33 @@
+1-
+(
+	
+SELECT first_name FROM actor
+
+)
+
+UNION
+
+(
+	
+SELECT first_name FROM customer
+
+);
+
+2-
+(
+	SELECT first_name FROM actor
+)
+INTERSECT
+(
+	SELECT first_name FROM customer
+);
+
+3-
+(
+	SELECT first_name FROM actor
+)
+EXCEPT
+(
+	SELECT first_name FROM customer
+);
+
