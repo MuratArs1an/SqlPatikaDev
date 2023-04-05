76c153c85dba49131ad27af0cc5a40f57e26aa10
Murat ARSLAN <68780096+MuratArs1an@users.noreply.github.com>
2023-04-06 01:09:22 +0300

    Add files via upload

---
+++ HomeWork12.sql
@@ -0,0 +1,35 @@
+1-
+SELECT COUNT(film) FROM film
+
+WHERE length>
+(
+
+SELECT AVG(length) FROM film
+
+);
+
+2-
+SELECT COUNT(film) FROM film
+WHERE rental_rate=(
+SELECT MAX(rental_rate) FROM film
+);
+
+3-
+SELECT * FROM film
+WHERE rental_rate =
+(
+    SELECT MIN(rental_rate) FROM film
+)
+AND replacement_cost =
+(
+    SELECT MIN(replacement_cost) FROM film
+);
+
+4-
+SELECT (
+SELECT first_name FROM customer
+	WHERE payment.customer_id=customer.customer_id
+),
+COUNT(amount) AS most_purchases FROM payment
+GROUP BY first_name
+ORDER BY most_purchases DESC;
